bd0511ea8b05786649a87102db7e3a4d2e6fcf6d
naiden00 <naiden.petrov.31.12.00@gmail.com>
2024-08-26 13:16:19 +0300

    .

--- MistralAI/index.js
+++ MistralAI/index.js
@@ -1,7 +1,13 @@
+import { readFile } from "fs/promises";
+import { join } from "path";
 import { Mistral } from "@mistralai/mistralai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+//Windows
+// const apiKey = process.env.MISTRAL_AI_API_KEY;
+//Linux
+const apiKey = process.env.MISTRAL_AI_API_KEY;
+const client = new Mistral({ apiKey });
 const main = async () => {
-    const apiKey = process.env.MISTRAL_AI_API_KEY;
-    const client = new Mistral({ apiKey });
     const chatResponse = await client.chat.complete({
         model: "mistral-tiny",
         messages: [
@@ -18,6 +24,30 @@ const main = async () => {
     //   for await (const chunk of chatResponse) {
     //     chunk.data.choices[0].delta.content;
     //   }
-    console.log(chatResponse.choices[0].message);
+    if (chatResponse && chatResponse.choices) {
+        console.log(chatResponse.choices[0].message);
+    }
 };
-main();
+async function splitDocument(path) {
+    try {
+        const filePath = join(path);
+        const text = await readFile(filePath, "utf8");
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 250,
+            chunkOverlap: 40,
+        });
+        const output = await splitter.createDocuments([text]);
+        return output.map((chunk) => chunk.pageContent);
+    }
+    catch (error) {
+        console.error("Error reading file:", error);
+    }
+}
+// await splitDocument("handbook.txt");
+// main();
+const exampleChunk = "professional ethics and behavior are expected of all Ambrics employees. Further, Ambrics expects each employee to display good judgment,";
+const response = await client.embeddings.create({
+    model: "mistral-embed",
+    inputs: [exampleChunk],
+});
+console.log(response);
--- MistralAI/index.ts
+++ MistralAI/index.ts
@@ -1,10 +1,15 @@
+import { readFile } from "fs/promises";
+import { join } from "path";
 import { Mistral } from "@mistralai/mistralai";
-import { isUndefined } from "util";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+//Windows
+// const apiKey = process.env.MISTRAL_AI_API_KEY;
+//Linux
+const apiKey = process.env.MISTRAL_AI_API_KEY;
+const client = new Mistral({ apiKey });
 
 const main = async () => {
-  const apiKey = process.env.MISTRAL_AI_API_KEY;
-  const client = new Mistral({ apiKey });
-
   const chatResponse = await client.chat.complete({
     model: "mistral-tiny",
     messages: [
@@ -23,7 +28,33 @@ const main = async () => {
   //     chunk.data.choices[0].delta.content;
   //   }
 
-  console.log(chatResponse.choices[0].message);
+  if (chatResponse && chatResponse.choices) {
+    console.log(chatResponse.choices[0].message);
+  }
 };
 
-main();
+async function splitDocument(path: string) {
+  try {
+    const filePath = join(path);
+    const text = await readFile(filePath, "utf8");
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 250,
+      chunkOverlap: 40,
+    });
+    const output = await splitter.createDocuments([text]);
+    return output.map((chunk) => chunk.pageContent);
+  } catch (error) {
+    console.error("Error reading file:", error);
+  }
+}
+// await splitDocument("handbook.txt");
+// main();
+
+const exampleChunk =
+  "professional ethics and behavior are expected of all Ambrics employees. Further, Ambrics expects each employee to display good judgment,";
+const response = await client.embeddings.create({
+  model: "mistral-embed",
+  inputs: [exampleChunk],
+});
+console.log(response);
+
