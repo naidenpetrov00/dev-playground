77b853871c04706df6db1cff9da6f69d4e22ca21
Nayden <naiden.petrov.31.12.00@gmail.com>
2024-10-05 20:39:43 +0300

    Add Redis Exercise

---
+++ RedisDemoProject/src/Gatherly.Persistence/PrivateResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Gatherly.Domain.Entities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Gatherly.Persistence;
+
+public class PrivateResolver : DefaultContractResolver
+{
+    protected override JsonProperty CreateProperty(
+        MemberInfo member,
+        MemberSerialization memberSerialization
+    )
+    {
+        JsonProperty prop = base.CreateProperty(member, memberSerialization);
+
+        if (!prop.Writable)
+        {
+            var property = member as PropertyInfo;
+            bool hasePrivateSetter = property?.GetSetMethod(true) != null;
+
+            prop.Writable = hasePrivateSetter;
+        }
+
+        return prop;
+    }
+}
---
+++ RedisDemoProject/src/Gatherly.Persistence/Repositories/CachedMemberRepository.cs
@@ -0,0 +1,74 @@
+﻿using Gatherly.Domain.Entities;
+using Gatherly.Domain.Repositories;
+using Gatherly.Domain.ValueObjects;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+
+namespace Gatherly.Persistence.Repositories;
+
+public class CachedMemberRepository : IMemberRepository
+{
+    private readonly IMemberRepository _decorated;
+    private readonly IDistributedCache distributedCache;
+    private readonly ApplicationDbContext dbContext;
+
+    public CachedMemberRepository(
+        IMemberRepository decorated,
+        IDistributedCache distributedCache,
+        ApplicationDbContext dbContext
+    )
+    {
+        _decorated = decorated;
+        this.distributedCache = distributedCache;
+        this.dbContext = dbContext;
+    }
+
+    public async Task<Member?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        string key = $"member-{id}";
+
+        string cachedMember = await distributedCache.GetStringAsync(key, cancellationToken);
+
+        Member? member;
+        if (string.IsNullOrEmpty(cachedMember))
+        {
+            member = await _decorated.GetByIdAsync(id, cancellationToken);
+            if (member == null)
+            {
+                return null;
+            }
+
+            await distributedCache.SetStringAsync(
+                key,
+                JsonConvert.SerializeObject(member),
+                cancellationToken
+            );
+
+            return member;
+        }
+
+        member = JsonConvert.DeserializeObject<Member>(
+            cachedMember,
+            new JsonSerializerSettings
+            {
+                ConstructorHandling = ConstructorHandling.Default,
+                ContractResolver = new PrivateResolver(),
+            }
+        );
+        if (member != null)
+        {
+            dbContext.Set<Member>().Attach(member);
+        }
+        return member;
+    }
+
+    public Task<bool> IsEmailUniqueAsync(
+        Email email,
+        CancellationToken cancellationToken = default
+    ) => _decorated.IsEmailUniqueAsync(email, cancellationToken);
+
+    public void Add(Member member) => _decorated.Add(member);
+
+    public void Update(Member member) => _decorated.Update(member);
+}
