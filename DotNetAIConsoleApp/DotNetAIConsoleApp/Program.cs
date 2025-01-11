189651d8b91ef954837c8b2145c4fd5fc469af66
naiden00 <naiden.petrov.31.12.00@gmail.com>
2025-01-11 19:31:24 +0200

    Open AI Test

---
+++ DotNetAIConsoleApp/DotNetAIConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.SemanticKernel;
+
+var apiKey = Environment.GetEnvironmentVariable("OpenApi");
+
+var kernel = Kernel.CreateBuilder().AddOpenAIChatCompletion("gpt-3.5-turbo", apiKey!).Build();
+
+while (true)
+{
+    Console.WriteLine("Q: ");
+    var question = Console.ReadLine();
+    System.Console.WriteLine(await kernel.InvokePromptAsync(question!));
+}
