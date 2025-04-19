d699494b5bf5dcac830baf8ec0c5c9ca326c44cf
Nayden <naiden.petrov.31.12.00@gmail.com>
2025-04-19 15:10:56 +0300

    Snapshot testing finish

--- DotNet/SnapshotTesting/Test/PackingListTests.cs
+++ DotNet/SnapshotTesting/Test/PackingListTests.cs
@@ -1,14 +1,37 @@
 ﻿using TestConsoleAppTemplate.Data;
+using TestConsoleAppTemplate.Models;
+using TestConsoleAppTemplate.Services;
 
 namespace SnapshotTesting.Test;
 
 public class PackPackingListTestsingList
 {
+    private PackingListService _packingListService;
+    private List<PackingList> _packingListsForMan;
+    private readonly VerifySettings _verifySettings;
+
+    public PackPackingListTestsingList()
+    {
+        _packingListService = new PackingListService();
+        _packingListsForMan = PackingLists.GetPackingListForMan();
+        _verifySettings = new VerifySettings();
+        _verifySettings.UseDirectory("snapshots");
+    }
+
     [Fact]
     public async Task GetPackingListForMan_Returns_Two_PackingLists()
     {
         var packingListForMan = PackingLists.GetPackingListForMan();
 
-        await Verify(packingListForMan);
+        await Verify(packingListForMan, _verifySettings);
+    }
+
+    [Fact]
+    public async Task GetPackingListByName_Throws_ArgumentException_When_PackingList_NotFound()
+    {
+        var action = () =>
+            _packingListService.GetPackingListByName("NotSuchName", _packingListsForMan);
+
+        await Throws(action, _verifySettings);
     }
 }
---
+++ DotNet/SnapshotTesting/Test/VerifyChecksTests.cs
@@ -0,0 +1,7 @@
+namespace SnapshotTesting.Test;
+
+public class VerifyChecksTests
+{
+    [Fact]
+    public Task Run() => VerifyChecks.Run();
+}
